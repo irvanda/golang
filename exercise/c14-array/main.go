2f195f98e1508de6b36644e64a6b2e61c1c5b355
Irvanda Kurniadi Virdaus <irvanda@evos.sg>
2020-02-01 01:34:08 +0700

    exercise: function as parameter

--- exercise/c14-array/main.go
+++ exercise/c14-array/main.go
@@ -8,7 +8,7 @@ func main() {
 	names[1] = "satu"
 	names[2] = "dua"
 
-	var fruits = [3]string{"apple", "orange", "pear"}
+	var fruits = []string{"apple", "orange", "pear"}
 
 	fmt.Println("jumlah element: \t\t", len(names))
 	fmt.Println("isi semua element: \t", fruits)
@@ -18,8 +18,79 @@ func main() {
 	fmt.Println("data array: \t", numbers)
 
 	var numbers1 = [2][3]int{[...]int{1, 2, 3}, [...]int{4, 5, 6}}
-	var numbers2 = [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	var numbers2 = [][]int{{1, 2, 3}, {4, 5, 6}}
 
 	fmt.Println("numbers1: ", numbers1)
 	fmt.Println("numbers2: ", numbers2)
+
+	fmt.Println(fruits[0])
+
+	// slice
+
+	var newFruits = fruits[0:2]
+	fmt.Println(newFruits)
+
+	var cFruits = append(fruits, "papaya")
+	fmt.Println(cFruits)
+
+	dst := make([]string, 2)
+	src := []string{"satu", "dua", "tiga", "empat"}
+	n := copy(dst, src)
+
+	fmt.Println(dst)
+	fmt.Println(src)
+	fmt.Println(n)
+
+	// map
+
+	var chicken map[string]int
+	chicken = map[string]int{}
+
+	chicken["satu"] = 1
+	chicken["dua"] = 2
+
+	fmt.Println(chicken)
+
+	var chicken2 = map[string]int{
+		"januari":  50,
+		"februari": 40,
+		"maret":    34,
+		"april":    67,
+	}
+
+	for key, val := range chicken2 {
+		fmt.Println(key, "  \t:", val)
+	}
+
+	fmt.Println()
+	delete(chicken2, "januari")
+	for key, val := range chicken2 {
+		fmt.Println(key, "  \t:", val)
+	}
+
+	// map item detection
+	fmt.Println()
+
+	chicken = map[string]int{"januari": 50, "februari": 40}
+	var value, isExist = chicken["mei"]
+
+	if isExist {
+		fmt.Println(value)
+	} else {
+		fmt.Println("item is not exists")
+	}
+
+	// combine slice & map
+	fmt.Println()
+
+	var chickens = []map[string]string{
+		map[string]string{"name": "chicken blue", "gender": "male"},
+		map[string]string{"name": "chicken red", "gender": "male"},
+		map[string]string{"name": "chicken yellow", "gender": "female"},
+	}
+
+	for _, chicken := range chickens {
+		fmt.Println(chicken["gender"], chicken["name"])
+	}
+
 }
---
+++ exercise/c17-function/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	var names = []string{"John", "Wick"}
+	printMessage("halo", names)
+
+	var randomValue int
+	fmt.Println()
+
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number:", randomValue)
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number:", randomValue)
+	randomValue = randomWithRange(2, 10)
+	fmt.Println("random number:", randomValue)
+	fmt.Println("time (unix): ", time.Now().Unix())
+
+	divideNumber(10, 2)
+	divideNumber(4, 0)
+	divideNumber(8, -4)
+
+	var diameter float64 = 15
+	var area, circumference = calculate(diameter)
+
+	fmt.Printf("luas lingkaran\t\t: %.2f \n", area)
+	fmt.Printf("keliling lingkaran\t: %.2f \n", circumference)
+
+	var avg = calculate2(2, 4, 3, 5, 4, 3, 3, 5, 5, 3)
+	var msg = fmt.Sprintf("Rata-rata : %.2f", avg)
+	fmt.Println(msg)
+
+	// fungsi closure
+	var getMinMax = func(n []int) (int, int) {
+		var min, max int
+		for i, e := range n {
+			switch {
+			case i == 0:
+				max, min = e, e
+			case e > max:
+				max = e
+			case e < min:
+				min = e
+			}
+		}
+		return min, max
+	}
+
+	var numbers = []int{2, 3, 4, 3, 4, 2, 3}
+	var min, max = getMinMax(numbers)
+	fmt.Printf("data : %v\nmin  : %v\nmax  : %v\n", numbers, min, max)
+
+	numbers = []int{2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
+
+	var newNumbers = func(min int) []int {
+		fmt.Println("min number :", min)
+		var r []int
+		for _, e := range numbers {
+			if e < min {
+				continue
+			}
+			r = append(r, e)
+		}
+		return r
+	}(3)
+
+	fmt.Println("original number :", numbers)
+	fmt.Println("filtered number :", newNumbers)
+}
+
+func randomWithRange(min, max int) int {
+	var value = rand.Int()%(max-min+1) + min
+	return value
+}
+
+func printMessage(message string, arr []string) {
+	var nameString = strings.Join(arr, " ")
+	fmt.Println(message, nameString)
+}
+
+func divideNumber(m, n int) {
+	if n == 0 {
+		fmt.Printf("invalid divider. %d cannot divided by %d\n", m, n)
+		return
+	}
+
+	var res = m / n
+	fmt.Printf("%d / %d = %d\n", m, n, res)
+}
+
+func calculate(d float64) (float64, float64) {
+	// hitung luas
+	var area = math.Pi * math.Pow(d/2, 2)
+	// hitung keliling
+	var circumference = math.Pi * d
+
+	// kembalikan 2 nilai
+	return area, circumference
+}
+
+// fungsi variadic
+func calculate2(numbers ...int) float64 {
+	var total int = 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	var avg = float64(total) / float64(len(numbers))
+	return avg
+}
